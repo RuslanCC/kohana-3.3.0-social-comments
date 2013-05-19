ecd20a2bfe7d69c19b2e4ff6842a034cdd1fbeec
RuslanCC <mail@ruslan.cc>
2013-05-19 09:34:29 +0400

    Initial commit

---
+++ comments/config/comments.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array
+(
+	// Ключ api вконтакте
+	"vk_key" 		=> "ВАШ_КЛЮЧ",
+	// Кол-во отображаемых сообщений вконтакте
+	"vk_messages" 	=> "15",
+	// Ширина виджета вконтакте, в пикселях
+	"vk_width" 		=> "470",
+	
+	// Кол-во отображаемых сообщений facebook
+	"fb_messages" 	=> "15",
+	// Ширина виджета facebook, в пикселях
+	"fb_width" 		=> "470",
+	
+);
